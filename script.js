9afba27df1e365faabb44456d6ce850ddff5d2e5
johnmboudreaux <jhnbdrx@gmail.com>
2017-07-16 16:33:13 -0600

    contructor function working. cards prepending

--- script.js
+++ script.js
@@ -1,95 +1,140 @@
-var $saveButton = $('.save-btn');
-var $newCard = $('.card');
-var $qualityText;
+var $saveBtn = $('.save-btn');
+var $card = $('.card');
+var $ideaTitle = $('#idea-title');
+var $ideaBody = $('#idea-info');
+var cardArray = [];
 
-console.log('working1');
-$saveButton.on('click', addCard);
+var IndexCard = function(title, body, id) {
+  this.title = title;
+  this.body = body;
+  this.quality = 'swill';
+  this.id = id;
+}
 
-function addCard(e) {
+IndexCard.prototype.build = function() {
+       $('.bottom-container').prepend(
+         `<article id="${this.id}" class="card">
+           <h3 class="card-title">${this.title}</h3>
+           <img class="delete" src="assets/delete.svg" alt="delete button" />
+           <p class="card-text">${this.body}</p>
+           <img class="up-vote" src="assets/upvote.svg" alt="up-vote button" />
+           <img class="down-vote" src="assets/downvote.svg" alt="down-vote button" />
+           <p class="quality">quality: <span id="quality-text">${this.quality}</span></p>
+         </article>`
+       );
+   }
 
-  e.preventDefault();
-  console.log('working2');
+   $saveBtn.click(function(e){
+     e.preventDefault();
+     var title = $ideaTitle.val();
+     var body = $ideaBody.val();
 
-  var $bottomContainer = $('.bottom-container');
-  var $ideaTitle = $('#idea-title').val();
-  var $ideaInfo = $('#idea-info').val();
+     console.log(title);
+     console.log(body);
+     var newIndexCard = new IndexCard(title, body);
+     newIndexCard.build();
+     cardArray.push(newIndexCard);
+     console.log(cardArray);
+});
 
-  $newCard = `<article class="card">
-        <h3 class="card-title">${$ideaTitle}</h3>
-        <img class="delete" src="assets/delete.svg" alt="delete button" />
-        <p class="card-text">${$ideaInfo}</p>
-        <img class="up-vote" src="assets/upvote.svg" alt="up-vote button" />
-        <img class="down-vote" src="assets/downvote.svg" alt="down-vote button" />
-        <p class="quality">quality: <span id="quality-text">swill</span></p>
-      </article>`
 
-  $bottomContainer.prepend($newCard);
+//  IndexCard.prototype.upvote = function () {
+//      this.upvotes ++;
+//  },
+ //
+//  IndexCard.prototype.downvote = function () {
+//      this.upvotes --;
+//  }
 
-  $('.delete').on('click', deleteCard);
-
-  $('.up-vote').on('click', upvoteFunction);
-
-  $('.down-vote').on('click', downvoteFunction);
-
-};
-
-function deleteCard () {
-  console.log('working3');
-  console.log($(this));
-  $(this).parent().remove();
-};
-
-function upvoteFunction () {
-  var $replaceText = $('#quality-text');
-  $qualityText = $('#quality-text').text();
-  console.log($qualityText);
-  if ($qualityText === 'swill') {
-    console.log($('if'));
-    $replaceText.text('plausible')
-  } else if ($qualityText === `plausible`) {
-    //  ($(this).attr('src',"assets/upvote-hover.svg"))
-    // can us this code for each if else statement to enable
-    // hovered states for buttons remain active when clicked
-    console.log($('else if'));
-    $replaceText.text('genius')
-  }
-};
-
-function downvoteFunction () {
-  var $replaceText = $('#quality-text');
-  $qualityText = $('#quality-text').text();
-  console.log($qualityText);
-  if ($qualityText === 'genius') {
-    console.log($('if'));
-    $replaceText.text('plausible')
-  } else if ($qualityText === `plausible`) {
-  //  ($(this).attr('src',"assets/upvote-hover.svg"))
-  // can us this code for each if else statement to enable
-  // hovered states for buttons remain active when clicked
-    console.log($('else if'));
-    $replaceText.text('swill')
-  }
-};
-
-// NEED TO REFACTOR, THIS FUNCTION CAN BE PULL OUT OF ADDCARD
-
-// $(function() {
-//  $('.up-vote').click(function(){
-//    $(this).attr('src',"assets/upvote-hover.svg");
-//     });
-//   });
 //
-// $('.list-toggle').click(function() {
-//   if ($('.list-sort').attr('colspan')) {
-//       $('.list-sort').removeAttr('colspan');
-//   } else {
-//       $('.list-sort').attr('colspan', 6);
+// console.log('working1');
+// $saveButton.on('click', addCard);
+//
+// function addCard(e) {
+//
+//   e.preventDefault();
+//   console.log('working2');
+//
+//   var $bottomContainer = $('.bottom-container');
+//   var $ideaTitle = $('#idea-title').val();
+//   var $ideaInfo = $('#idea-info').val();
+//
+//   $newCard = `<article class="card">
+//         <h3 class="card-title">${$ideaTitle}</h3>
+//         <img class="delete" src="assets/delete.svg" alt="delete button" />
+//         <p class="card-text">${$ideaInfo}</p>
+//         <img class="up-vote" src="assets/upvote.svg" alt="up-vote button" />
+//         <img class="down-vote" src="assets/downvote.svg" alt="down-vote button" />
+//         <p class="quality">quality: <span id="quality-text">swill</span></p>
+//       </article>`
+//
+//   $bottomContainer.prepend($newCard);
+//
+//   $('.delete').on('click', deleteCard);
+//
+//   $('.up-vote').on('click', upvoteFunction);
+//
+//   $('.down-vote').on('click', downvoteFunction);
+//
+// };
+//
+// function deleteCard () {
+//   console.log('working3');
+//   console.log($(this));
+//   $(this).parent().remove();
+// };
+//
+// function upvoteFunction () {
+//   var $replaceText = $('#quality-text');
+//   $qualityText = $('#quality-text').text();
+//   console.log($qualityText);
+//   if ($qualityText === 'swill') {
+//     console.log($('if'));
+//     $replaceText.text('plausible')
+//   } else if ($qualityText === `plausible`) {
+//     //  ($(this).attr('src',"assets/upvote-hover.svg"))
+//     // can us this code for each if else statement to enable
+//     // hovered states for buttons remain active when clicked
+//     console.log($('else if'));
+//     $replaceText.text('genius')
 //   }
-// });
-
-$saveButton.on('click', clearInputFields);
-
-function clearInputFields() {
-  $('#idea-title').val('');
-  $('#idea-info').val('');
-};
+// };
+//
+// function downvoteFunction () {
+//   var $replaceText = $('#quality-text');
+//   $qualityText = $('#quality-text').text();
+//   console.log($qualityText);
+//   if ($qualityText === 'genius') {
+//     console.log($('if'));
+//     $replaceText.text('plausible')
+//   } else if ($qualityText === `plausible`) {
+//   //  ($(this).attr('src',"assets/upvote-hover.svg"))
+//   // can us this code for each if else statement to enable
+//   // hovered states for buttons remain active when clicked
+//     console.log($('else if'));
+//     $replaceText.text('swill')
+//   }
+// };
+//
+// // NEED TO REFACTOR, THIS FUNCTION CAN BE PULL OUT OF ADDCARD
+//
+// // $(function() {
+// //  $('.up-vote').click(function(){
+// //    $(this).attr('src',"assets/upvote-hover.svg");
+// //     });
+// //   });
+// //
+// // $('.list-toggle').click(function() {
+// //   if ($('.list-sort').attr('colspan')) {
+// //       $('.list-sort').removeAttr('colspan');
+// //   } else {
+// //       $('.list-sort').attr('colspan', 6);
+// //   }
+// // });
+//
+// $saveButton.on('click', clearInputFields);
+//
+// function clearInputFields() {
+//   $('#idea-title').val('');
+//   $('#idea-info').val('');
+// };
