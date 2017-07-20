72c6930c42ee9635e6558763732c7d3d22907354
Amy Holt <amy.elizabeth.holt@gmail.com>
2017-07-20 08:58:22 -0600

    organized js

--- script.js
+++ script.js
@@ -12,7 +12,14 @@ var IndexCard = function (title, body, id) {
   this.id = id || Date.now();
 };
 
+$saveBtn.on('click', clickSave);
+$('.bottom-container').on('click', '.up-vote', upVote);
+$('.bottom-container').on('click', '.down-vote', downVote);
+$ideaBody.on('input', saveBtnOn);
+$('.search').on('keyup', runSearch);
+
 populateIndexCardArray();
+populateDOM();
 
 function populateIndexCardArray() {
   var objectKeys = Object.keys(localStorage);
@@ -21,13 +28,26 @@ function populateIndexCardArray() {
   });
 }
 
-populateDOM();
-
 function populateDOM() {
   for (var i = 0; i < indexCardArray.length; i++) {
     build(indexCardArray[i]);
   }
 }
+
+function saveBtnOn() {
+  $saveBtn.attr('disabled', false);
+}
+
+function clickSave (e) {
+  e.preventDefault();
+  var title = $ideaTitle.val();
+  var body = $ideaBody.val();
+  var newIndexCard = new IndexCard(title, body);
+  build(newIndexCard);
+  indexCardArray.push(newIndexCard);
+  addIndexCardToLocalStorage(newIndexCard);
+  clearInputFields();
+};
 
 function build(newIndexCard) {
   var newTitle = newIndexCard.title;
@@ -43,11 +63,14 @@ function build(newIndexCard) {
      <div class="down-vote"></div>
      <p class="quality">quality: <span id="quality-text">${newQuality}</span></p>
    </article>`
-   $('.card-title').on('blur', updateTitle);
-   $('.card-text').on('blur', updateBody);
   );
   $('.card-title').on('blur', updateTitle);
   $('.card-text').on('blur', updateBody);
+}
+
+function addIndexCardToLocalStorage(newIndexCard) {
+  var stringifiedIndexCard = JSON.stringify(newIndexCard);
+  localStorage.setItem(newIndexCard.id, stringifiedIndexCard);
 }
 
 function clearInputFields() {
@@ -56,42 +79,11 @@ function clearInputFields() {
   $saveBtn.attr('disabled', true);
 }
 
-$saveBtn.on('click', clickSave);
-
-function clickSave (e) {
-  e.preventDefault();
-  var title = $ideaTitle.val();
-  var body = $ideaBody.val();
-  var newIndexCard = new IndexCard(title, body);
-  build(newIndexCard);
-  indexCardArray.push(newIndexCard);
-  addIndexCardToLocalStorage(newIndexCard);
-  clearInputFields();
-};
-
 $('.bottom-container').on('click', '.delete', function () {
   $(this).parent().remove();
   localStorage.removeItem($(this).parent().prop('id'));
 });
 
-$('.bottom-container').on('click', '.up-vote', upVote);
-
-function upVote() {
-  var $changeQuality = $(this).parent().find('span').text();
-  var id = $(this).parent().prop('id');
-  var specificCard = JSON.parse(localStorage.getItem(id));
-  if ($changeQuality === 'swill') {
-    $(this).parent().find('span').text('plausible');
-    specificCard.quality = 'plausible';
-    localStorage.setItem(id, JSON.stringify(specificCard));
-  } else if ($changeQuality === 'plausible') {
-    $(this).parent().find('span').text('genius');
-    specificCard.quality = 'genius';
-    localStorage.setItem(id, JSON.stringify(specificCard));
-  }
-}
-
-$('.bottom-container').on('click', '.down-vote', downVote);
 
 function downVote() {
   var $changeQuality = $(this).parent().find('span').text();
@@ -108,52 +100,19 @@ function downVote() {
   }
 }
 
-function addIndexCardToLocalStorage(newIndexCard) {
-  var stringifiedIndexCard = JSON.stringify(newIndexCard);
-  localStorage.setItem(newIndexCard.id, stringifiedIndexCard);
-}
-
-// function populateIndexCardArray() {
-//   var objectKeys = Object.keys(localStorage);
-//   objectKeys.forEach(function (uniqueId) {
-//     indexCardArray.push(JSON.parse(localStorage[uniqueId]));
-//   });
-// }
-//
-// function populateDOM() {
-//   for (var i = 0; i < indexCardArray.length; i++) {
-//     build(indexCardArray[i]);
-//   }
-// }
-
-$ideaBody.on('input', saveBtnOn);
-
-function saveBtnOn() {
-  $saveBtn.attr('disabled', false);
-}
-
-$('.search').on('keyup', runSearch);
-
-function runSearch() {
-  var search = $(this).val().toUpperCase();
-  var searchedArray = indexCardArray.filter(function (newIndexCard) {
-    return newIndexCard.title.toUpperCase().includes(search) || newIndexCard.body.toUpperCase().includes(search) || newIndexCard.quality.toUpperCase().includes(search);
-
-  });
-
-  $('.bottom-container').empty();
-  for (var i = 0; i < searchedArray.length; i++) {
-    build(searchedArray[i]);
-  }
-}
-
-function updateTitle() {
-  var $updatedTitle = $(this).parent().find('h3').text();
+function upVote() {
+  var $changeQuality = $(this).parent().find('span').text();
   var id = $(this).parent().prop('id');
   var specificCard = JSON.parse(localStorage.getItem(id));
-  console.log('updatetitle function running');
-  specificCard.title = $updatedTitle;
-  localStorage.setItem(id, JSON.stringify(specificCard));
+  if ($changeQuality === 'swill') {
+    $(this).parent().find('span').text('plausible');
+    specificCard.quality = 'plausible';
+    localStorage.setItem(id, JSON.stringify(specificCard));
+  } else if ($changeQuality === 'plausible') {
+    $(this).parent().find('span').text('genius');
+    specificCard.quality = 'genius';
+    localStorage.setItem(id, JSON.stringify(specificCard));
+  }
 }
 
 function updateBody() {
@@ -165,29 +124,22 @@ function updateBody() {
   localStorage.setItem(id, JSON.stringify(specificCard));
 }
 
+function updateTitle() {
+  var $updatedTitle = $(this).parent().find('h3').text();
+  var id = $(this).parent().prop('id');
+  var specificCard = JSON.parse(localStorage.getItem(id));
+  console.log('updatetitle function running');
+  specificCard.title = $updatedTitle;
+  localStorage.setItem(id, JSON.stringify(specificCard));
+}
 
-// $('.card-text').on('blur', editCardToStorage);
-
-// function editTitleToStorage() {
-//   var id = $(this).parent().prop('id');
-//   console.log('I am ID: '+id);
-//   var title = $(this).text();
-//   var specificCard = JSON.parse(localStorage.getItem(id));
-//   //console.log(indexCardArray[0]);
-//   // title = $(this).parent().find('h3').text();
-//   var index = indexCardArray.reduce( function( cur, val, index ){
-//
-//     if( val.id === id && cur === -1 ) {
-//         return index;
-//     }
-//     return cur;
-//
-// }, -1 );
-//   var index = indexCardArray.findIndex(i => i.id === id);
-//   console.log('I am Index: ' + index);
-//   // indexCardArray.indexOf(id);
-//   console.log(title);
-//   localStorage.setItem(id, JSON.stringify(specificCard));
-//   // var stringifiedEditedCard = JSON.stringify(newIndexCard);
-//   // localStorage.setItem(newIndexCard.title, stringifiedEditedCard);
-// }
+function runSearch() {
+  var search = $(this).val().toUpperCase();
+  var searchedArray = indexCardArray.filter(function (newIndexCard) {
+    return newIndexCard.title.toUpperCase().includes(search) || newIndexCard.body.toUpperCase().includes(search);
+  });
+  $('.bottom-container').empty();
+  for (var i = 0; i < searchedArray.length; i++) {
+    build(searchedArray[i]);
+  }
+}
